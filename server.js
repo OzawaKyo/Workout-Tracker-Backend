96829d3da121e411a671dbab75e282d808a8dcd1
Mohamed Amarcha <80904831+OzawaKyo@users.noreply.github.com>
2025-02-24 19:08:39 +0100

    db

--- server.js
+++ server.js
@@ -27,3 +27,5 @@ setupSwagger(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
