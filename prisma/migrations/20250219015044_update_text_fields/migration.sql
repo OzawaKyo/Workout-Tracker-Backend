18b34aa9ce9200c91b86b8ea4faa65927519f81a
Mohamed Amarcha <80904831+OzawaKyo@users.noreply.github.com>
2025-02-24 19:01:26 +0100

    host

--- prisma/migrations/20250219015044_update_text_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `exercise` MODIFY `images` TEXT NOT NULL,
-    MODIFY `instructions` TEXT NOT NULL,
-    MODIFY `primaryMuscles` TEXT NOT NULL,
-    MODIFY `secondaryMuscles` TEXT NOT NULL;
